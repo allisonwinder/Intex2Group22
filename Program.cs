b9784c65897c5564c500cba1415c34d155c64329
natopo99 <104119337+natopo99@users.noreply.github.com>
2023-04-11 13:30:56 -0600

    added csp header

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Intex2Group22.Controllers;
 using Intex2Group22.Data;
 using Intex2Group22.Models;
 using Microsoft.AspNetCore.Identity;
@@ -12,11 +13,12 @@ builder.Services.AddDbContext<intexmummiesContext>(options =>
 {
     options.UseNpgsql(connectionString);
 });
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
@@ -28,6 +30,8 @@ builder.Services.Configure<CookiePolicyOptions>(options =>
     options.MinimumSameSitePolicy = SameSiteMode.None;
 });
 
+
+//Password strength requirements
 builder.Services.Configure<IdentityOptions>(options =>
 {
     // Default Password settings.
@@ -53,12 +57,12 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseMiddleware<CspMiddleware>(); // add middleware for CSP header
+//app.Use(async (context, next) => {
+//    context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'");
 
-app.Use(async (context, next) => {
-    context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'");
-
-    await next();
-});
+//    await next();
+//});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
