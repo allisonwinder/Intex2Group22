1ace85b783ca95df0bbcbd2e1836de1a7cd99298
natopo99 <104119337+natopo99@users.noreply.github.com>
2023-04-12 12:25:27 -0600

    fixed some stuff

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Intex2Group22.Controllers
+{
+    public class RoleController : Controller
+    {
+        [Authorize(Policy = "EmployeeOnly")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
